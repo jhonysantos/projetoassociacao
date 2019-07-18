a098f10aee945ce4a67537b98c3b02ae8a740869
Jhony Santos <jhony.cemi@gmail.com>
2019-07-18 10:27:31 -0300

    Primeira atualização
    Inserindo o cabeçalho do site

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width,user-scalable=0" />
+	<title>Associação Produtores Rurais</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<header>
+		<div class="containertopo">
+			<div class="contatotopo">
+				<div>
+					<img src="assets/images/whatsapp.png" /> (00)0000-0000
+				</div>
+				<div>
+					<img src="assets/images/envelope.png" /> associacao@gmail.com
+				</div>
+			</div>
+			<div class="logintopo">
+				<div class="associadotopo">
+					<a href="login.php">Área do associado</a>
+				</div>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
